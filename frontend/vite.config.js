3f1dd47a389857ef330350b4bd65f8c7a23ee592
Osman <osmaancizudiin@gmail.com>
2025-01-04 15:30:33 +0300

    node project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Specify a port (e.g., 3000 or any other available port)
+    host: '0.0.0.0', // Use 'localhost' or '0.0.0.0' to bind to IPv4
+  },
 })
